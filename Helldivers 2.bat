da63c02c5725a77a70e771851835aecec8d5f0e6
goncaloestrelado <goncalo.estrelado@hotmail.com>
2026-02-09 16:01:03 +0000

    Remove Helldivers 2.bat

--- Helldivers 2.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:: Start the Helldivers 2 Macro
-start "" "C:\Users\Utilizador\Desktop\HelldiversMacro\dist\main.exe"
-
-:: Launch Helldivers 2 via Steam
-start steam://rungameid/553850
-
-exit
